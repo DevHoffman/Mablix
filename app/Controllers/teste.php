f451b827e55bd78c2d0d1d68f45de4397b3e45b1
DevHoffman <53531808+DevHoffman@users.noreply.github.com>
2019-12-25 23:47:48 -0300

    Mablix Code Igniter

---
+++ app/Controllers/teste.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Teste extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
